2e7c0baf7abccaf21c0c83b2fca5b4decfa13c7c
ionorins <ion.orins@modex.tech>
2021-11-14 18:06:28 +0000

    push in order to test on remote machine

--- graph_recsys_benchmark/solvers.py
+++ graph_recsys_benchmark/solvers.py
@@ -259,7 +259,7 @@ class BaseSolver(object):
                                 with torch.no_grad():
                                     HRs, NDCGs, AUC, eval_loss = self.metrics(run, epoch, model, dataset)
 
-                                print(model.forward())
+                                print(len(model.forward()))
                                 exit()
                                 model.register_ewc_params(dataset.train_data)
                                 torch.save(model, 'model.pth')
