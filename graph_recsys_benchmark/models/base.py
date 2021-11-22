f0847c3e094c404ba5a4be3904c69decf6d61cbe
ionorins <ion.orins@modex.tech>
2021-11-22 13:56:43 +0000

    push in order to test on remote machine

--- graph_recsys_benchmark/models/base.py
+++ graph_recsys_benchmark/models/base.py
@@ -272,10 +272,6 @@ class PEABaseRecsysModel(GraphRecsysModel):
             glorot(self.att)
 
     def forward(self, metapath_idx=None):
-        print('forward' * 10)
-        print(metapath_idx)
-        print(self.channel_aggr)
-
         x = self.x
         x = [module(x, self.meta_path_edge_index_list[idx]).unsqueeze(1)
              for idx, module in enumerate(self.pea_channels)]
