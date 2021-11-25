ba55832471a70f5d070d91f0a556c1a54f1a5624
ionorins <ion.orins@modex.tech>
2021-11-25 16:32:54 +0000

    push in order to test on remote machine

--- graph_recsys_benchmark/solvers.py
+++ graph_recsys_benchmark/solvers.py
@@ -222,7 +222,7 @@ class BaseSolver(object):
                                 print('dataset:')
                                 print(dataset.train_data)
 
-                                mask = [x.item() in set for x in dataset.train_data[:, 0]]
+                                mask = [x.item() in last_nodes for x in dataset.train_data[:, 0]]
                                 dataset.train_data = dataset.train_data[mask]
 
                                 
