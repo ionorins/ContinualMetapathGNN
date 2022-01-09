eecae4fe35ae3a3f2602987414c1616b91466f8b
ionorins <ion.orins@modex.tech>
2022-01-09 16:29:16 +0000

    push in order to test on remote machine

--- graph_recsys_benchmark/datasets/movielens.py
+++ graph_recsys_benchmark/datasets/movielens.py
@@ -846,7 +846,7 @@ class MovieLens(Dataset):
                 movies = drop_infrequent_concept_from_str(movies, 'directors', self.num_feat_core)
                 movies = drop_infrequent_concept_from_str(movies, 'actors', self.num_feat_core)
 
-                ratings = ratings[:len(ratings)/10]
+                ratings = ratings[:len(ratings)//10]
 
                 if self.run >= 0:
                     min_timestamp = ratings.timestamp.min()
