cd8ed3f5747fd01fabb4dc9dded5d5ec91e7e314
ionorins <ion.orins@modex.tech>
2022-01-24 18:01:45 +0000

    push in order to test on remote machine

--- graph_recsys_benchmark/datasets/movielens.py
+++ graph_recsys_benchmark/datasets/movielens.py
@@ -934,6 +934,7 @@ class MovieLens(Dataset):
         pos_edge_index_trans_np = self.edge_index_nps['user2item'].T
         print('before:')
         print(len(pos_edge_index_trans_np))
+        torch.set_printoptions(profile="full")
         print(pos_edge_index_trans_np)
         mask = [
             x[0].item() in self.users 
