edb7e4fdff18b1ee36e819a0723efff9e9e00c0e
ionorins <ion.orins@modex.tech>
2022-01-27 09:52:44 +0000

    push in order to test on remote machine

--- graph_recsys_benchmark/datasets/movielens.py
+++ graph_recsys_benchmark/datasets/movielens.py
@@ -923,12 +923,9 @@ class MovieLens(Dataset):
         print(len(pos_edge_index_trans_np))
 
         def h(e):
-            print(e)
-            e0 = diff[e[0].item()]
-            e1 = diff[e[1].item()]
-            he = e0 + e1
-            print(he)
-            return torch.norm(he)
+            h0 = diff[e[0].item().int()]
+            h1 = diff[e[1].item().int()]
+            return torch.norm(h0 + h1)
 
 
         if diff is not None:
