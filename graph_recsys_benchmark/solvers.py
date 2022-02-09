dc1c1c9100af1a59bfe8bf0a5dc3f4f8d14659ec
ionorins <ion.orins@modex.tech>
2022-02-09 16:39:43 +0000

    push in order to test on remote machine

--- graph_recsys_benchmark/solvers.py
+++ graph_recsys_benchmark/solvers.py
@@ -167,7 +167,7 @@ class BaseSolver(object):
                             self.model_args['num_users'] = dataset.num_uids
                             self.model_args['num_items'] = dataset.num_iids
 
-                        if i == 0 or dataset.continual_aspect == 'single':
+                        if i == 0 or dataset.continual_aspect in ['single', 'retrained']:
                             model = self.model_class(**self.model_args)
                             model = model.to(self.train_args['device'])
                             last_emb = None
