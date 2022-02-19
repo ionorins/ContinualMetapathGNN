0cf0bdc443f023aa3c99407f0c1c35abdc82f007
ionorins <ion.orins@modex.tech>
2022-02-19 17:11:41 +0000

    push in order to test on remote machine

--- graph_recsys_benchmark/datasets/movielens.py
+++ graph_recsys_benchmark/datasets/movielens.py
@@ -998,7 +998,7 @@ class MovieLens(Dataset):
 
                 # pos_edge_index_trans_np = pos_edge_index_trans_np[:no_samples]
                 eps = 1e-4
-                T = 100
+                T = 1
                 imps = torch.tensor([h(e) for e in pos_edge_index_trans_np_old])/T + eps
                 print(imps)
                 p = torch.softmax(imps, dim=0)
