65095062033f442849de9607cc889ae7647dc32e
ionorins <ion.orins@modex.tech>
2022-02-19 18:02:10 +0000

    push in order to test on remote machine

--- graph_recsys_benchmark/datasets/movielens.py
+++ graph_recsys_benchmark/datasets/movielens.py
@@ -980,7 +980,7 @@ class MovieLens(Dataset):
         def age(e):
             n0 = int(e[0].item())
             n1 = int(e[1].item())
-            return self.timeframe - self.edge_last_use[(n0, n1)]
+            return self.timeframe - self.edge_last_use.get((n0, n1), 0)
 
         if last_emb is not None and self.continual_aspect == 'continual':
             if epoch == 1:
