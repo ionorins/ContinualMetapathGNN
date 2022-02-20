e5e267a7b2d2f0d5663d6a86bf616b4f108c6178
ionorins <ion.orins@modex.tech>
2022-02-20 14:40:44 +0000

    push in order to test on remote machine

--- graph_recsys_benchmark/datasets/movielens.py
+++ graph_recsys_benchmark/datasets/movielens.py
@@ -1004,8 +1004,8 @@ class MovieLens(Dataset):
                 ])
 
                 # pos_edge_index_trans_np = pos_edge_index_trans_np[:no_samples]
-                eps = 2**(-32)
-                T = 2**8
+                eps = 2
+                T = 2**16
                 b = 1
 
                 imps = torch.tensor([h(e) * (b ** age(e)) for e in pos_edge_index_trans_np_old], dtype=torch.double)
