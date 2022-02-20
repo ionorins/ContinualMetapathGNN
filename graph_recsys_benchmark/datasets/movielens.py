0f721053f775342174652067d10e7a7b967d9c0b
ionorins <ion.orins@modex.tech>
2022-02-20 22:00:45 +0000

    push in order to test on remote machine

--- graph_recsys_benchmark/datasets/movielens.py
+++ graph_recsys_benchmark/datasets/movielens.py
@@ -1001,7 +1001,7 @@ class MovieLens(Dataset):
 
                 rand_samp = pos_edge_index_trans_np[self.len_ratings + int(ro * no_samples):]
                 inds = np.random.choice(
-                    len(no_samples - int(ro * no_samples)), 
+                    no_samples - int(ro * no_samples), 
                     no_samples
                 )
                 rand_samp = rand_samp[inds]
