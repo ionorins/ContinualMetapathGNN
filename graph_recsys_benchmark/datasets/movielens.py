2258269dd688d5da5b908d732728cbc4afbd78f8
ionorins <ion.orins@modex.tech>
2022-02-21 22:41:03 +0000

    push in order to test on remote machine

--- graph_recsys_benchmark/datasets/movielens.py
+++ graph_recsys_benchmark/datasets/movielens.py
@@ -1048,7 +1048,7 @@ class MovieLens(Dataset):
                 b = 2
 
                 imps = torch.tensor([age(e) for e in pos_edge_index_trans_np_old], dtype=torch.double)
-                imps =  imps * T + eps
+                # imps =  imps * T + eps
                 print(f'imps: {imps}, min: {min(imps)}, max: {max(imps)}, mean: {torch.mean(imps)}')
                 p = torch.softmax(imps, dim=0)
                 p /= sum(p)
